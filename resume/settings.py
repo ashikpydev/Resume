31d35ecbe365157054078c0a883ae0b8d15d461a
ashikpydev <ashiqurrahman0506@gmail.com>
2021-05-04 16:05:55 +0600

    last updated

--- resume/settings.py
+++ resume/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wd=q$-ii9_)=6l^mm!odi*=wo6jor_35vt9x(09wsvbtee8=-r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- resumeapp/urls.py
+++ resumeapp/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path,include
 from .views import *
+from django.contrib import admin
+#django admin header customization
+admin.site.site_header = "Login to Developer Ashiq"
+admin.site.site_title = "Welcome to Asiq's Dashboard"
+admin.site.index_title = "Welcome to this Portal"
+
+
 urlpatterns = [
     path('', home),
 ]
